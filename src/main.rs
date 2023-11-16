ad0c82c0d82e2a81f62d6af3c33d6fc603d7956d
David L. Campbell <dcampbell24@gmail.com>
2023-11-16 15:10:21 -0500

    Initial backup script.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+//! Basic snapshot-style rsync backup script
+
+use std::{fs, io, process::Command, os};
+
+use chrono::{Utc, Datelike};
+
+fn main() -> Result<(), io::Error> {
+    let last = "/media/backup/ubuntu/last";
+
+    let mut backup = "/media/backup/ubuntu/".to_string();
+    let date = Utc::now();
+    backup = format!("{backup}{}-{}-{}", date.year(), date.month(), date.day());
+
+    Command::new("rsync")
+        .args([
+            "--archive",
+            "--partial",
+            "--progress",
+            "--human-readable",
+            "--link-dest=/media/backup/ubuntu/last/", 
+            "/home/ubuntu/",
+            &backup
+        ])
+        .status()
+        .expect("failed to execute rsync");
+
+    fs::remove_file(last)?;
+    os::unix::fs::symlink(&backup, last)?;
+    Ok(())
+}
